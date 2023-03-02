5aa504769c0c1a49ea2ffbc156d7fedbb7bb3c74
Ulrik Lyngs <ulrik.lyngs@gmail.com>
2023-03-02 16:47:10 +0000

    add optional delay to show the settings page

--- Shared (Extension)/Resources/popup.js
+++ Shared (Extension)/Resources/popup.js
@@ -2,6 +2,102 @@
 // https://developer.chrome.com/docs/extensions/mv3/messaging/
 
 document.addEventListener('DOMContentLoaded', function() {
+    // set friction checkbox
+    browser.storage.sync.get("addFriction", function(result) {
+      var frictionToggle = document.getElementById("frictionToggle");
+      var frictionCustomisation = document.querySelector(".friction-customisation");
+      
+      frictionToggle.checked = result["addFriction"];
+      
+        if (frictionToggle.checked) {
+            for (var i = 0; i < frictionCustomisation.length; i++) {
+                frictionCustomisation[i].style.display = "block";
+                }
+        } else {
+          // Otherwise, hide it
+            for (var i = 0; i < frictionCustomisation.length; i++) {
+                frictionCustomisation[i].style.display = "none";
+                }
+        }
+        
+      var popupContainer = document.getElementById("popup-content");
+      var messageContainer = document.getElementById("delay-content");
+            
+      var messageBox = document.getElementById("delay-message");
+      messageBox.innerText = "What is your intention?";
+            
+      if(result["addFriction"]) {
+        messageContainer.style.display = "block";
+        popupContainer.style.display = "none";
+          
+        browser.storage.sync.get("waitText").then(function(result) {
+            var waitText = result.waitText;
+            
+            if (waitText != null) {
+                // set the placeholder text
+                document.getElementById("waitText").placeholder = result.waitText;
+            } else {
+                document.getElementById("waitText").placeholder = "What's your intention?";
+            }
+        });
+                
+        browser.storage.sync.get("waitTime").then(function(result) {
+          var waitTime = result.waitTime;
+          var countdownBox = document.getElementById("delay-time");
+          var countdown = 0;
+                
+          if (waitTime != null) {
+              // set the placeholder text
+              document.getElementById("waitTime").placeholder = waitTime;
+              
+              // Use the wait time value for the count down
+              countdownBox.innerText = waitTime;
+              countdown = waitTime;            
+            
+          } else {
+            countdownBox.innerText = "10";
+            countdown = 10;
+          }
+                
+          // Update countdown timer every second
+          var timerId = setInterval(function() {
+            countdown--;
+            if (countdown >= 0) {
+              countdownBox.innerText = countdown;
+            } else {
+              // Hide the message box and stop the timer
+              messageContainer.style.display = "none";
+              
+              popupContainer.style.display = "block";
+              clearInterval(timerId);
+            }
+          }, 1000);
+        });
+      } else {
+        messageContainer.style.display = "none";
+        popupContainer.style.display = "block";
+          
+      }
+    });
+    
+    var frictionToggle = document.getElementById("frictionToggle");
+    var frictionCustomisation = document.querySelectorAll(".friction-customisation");
+    
+    // Add event listener to detect changes in the checkbox status
+    frictionToggle.addEventListener('change', function() {
+      // If the checkbox is checked, show the delay time input
+      if (frictionToggle.checked) {
+          for (var i = 0; i < frictionCustomisation.length; i++) {
+              frictionCustomisation[i].style.display = "block";
+              }
+      } else {
+        // Otherwise, hide it
+          for (var i = 0; i < frictionCustomisation.length; i++) {
+              frictionCustomisation[i].style.display = "none";
+              }
+      }
+    });
+    
     var elementsThatCanBeHidden = [ "youtubeRecVids",
                                     "youtubeThumbnails",
                                     "youtubeShorts",
@@ -70,6 +166,10 @@ document.addEventListener('DOMContentLoaded', function() {
             
             browser.storage.sync.set({ [key]: document.getElementById(element + "Toggle").checked });
         });
+        
+        browser.storage.sync.set({ "addFriction": document.getElementById("frictionToggle").checked });
+        browser.storage.sync.set({ "waitTime": parseInt(document.getElementById("waitTime").value) });
+        browser.storage.sync.set({ "waitText": parseInt(document.getElementById("waitText").value) });
     
         e.target.setAttribute("value", "......");
         delay(250).then(() => e.target.setAttribute("value", "Saved!"));
